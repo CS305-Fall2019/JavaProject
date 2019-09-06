50f33fcadc4bae013f3d6b6de3bbb1b4623ddeae
SIUKhaled <kahmed@cs.siu.edu>
2019-09-06 16:57:04 -0500

    Sample project in java to be cloned by CS305 Students

---
+++ PayrollDialog/src/PayrollDialog.java
@@ -0,0 +1,26 @@
+
+import javax.swing.JOptionPane;
+ 		/**
+ 			This program demonstrates using dialogs with
+ 			JOptionPane.  Converts string input to numeric. 		*/
+
+ 		public class PayrollDialog {
+ 			public static void main(String[] args) 	{
+ 				String inputString; 		// For reading input
+ 				String name; 			        // The user's name
+ 				int hours; 				// The number of hours worked
+ 				double payRate; 		        // The user's hourly pay rate
+ 				double grossPay; 		// The user's gross pay
+                name = JOptionPane.showInputDialog("What is"+	"your name?", "Dania");
+		        inputString  = JOptionPane.showInputDialog("How many hours " +
+									                           "did you work this week? ","40");
+                hours = Integer.parseInt(inputString);
+    			inputString = JOptionPane.showInputDialog("What is your " + "hourly pay rate? ","12");
+                payRate = Double.parseDouble(inputString);
+                       // Calculate the gross pay.
+           		grossPay = hours * payRate;
+			     JOptionPane.showMessageDialog(null, "Hello " +name + ". Your gross pay is $" +
+				                            				                           grossPay);
+			System.exit(0);
+ 			} 		
+ 		}
